6b1cbc2bac1419411e59b5e0835f6c1d5f6eee89
Miroslav Perovic <miroslav.perovic@gmail.com>
2012-12-01 12:55:41 +0100

    Initial Commit

---
+++ PAStepper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PAStepper
+//
+//  Created by Miroslav Perovic on 12/1/12.
+//  Copyright (c) 2012 Pure Agency. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PAStepper/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PAStepper
+//
+//  Created by Miroslav Perovic on 12/1/12.
+//  Copyright (c) 2012 Pure Agency. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
